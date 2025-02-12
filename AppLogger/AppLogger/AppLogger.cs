1a515eefe21665bae1467566a8ff5adc5cf20ef0
J W Eken <jweken@hetnet.nl>
2025-02-12 07:58:10 +0100

    Add project files.

---
+++ AppLogger/AppLogger/AppLogger.cs
@@ -0,0 +1,14 @@
+﻿// https://learn.microsoft.com/en-us/nuget/quickstart/create-and-publish-a-package-using-visual-studio?tabs=netcore-cli#prerequisites
+
+using System;
+
+namespace AppLogger
+{
+    public class Logger
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
